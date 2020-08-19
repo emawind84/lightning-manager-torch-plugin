b71c1d5b5a3ec5f085308f16202d87cd45fa5675
Emanuele Disco <emawind84@gmail.com>
2020-08-19 20:46:24 +0900

    update

---
+++ TestCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Torch.Commands;
+using Torch.Commands.Permissions;
+using VRage.Game.ModAPI;
+
+namespace MyFirstPlugin
+{
+
+    public class TestCommand : CommandModule
+    {
+
+        [Command("test", "This is a Test Command.")]
+        [Permission(MyPromoteLevel.Moderator)]
+        public void Test()
+        {
+
+        }
+
+    }
+}
